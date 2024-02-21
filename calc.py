d17f267f0a8cc8aca81c858df91d5a32a669e164
polyukhovich-olexandr <leanl3012@gmail.com>
2024-02-21 18:03:30 +0200

    initial calc.py & README.md

---
+++ calc.py
@@ -0,0 +1,23 @@
+# calc.py
+def add(a, b):
+    return float(a + b)
+
+
+def subtract(a, b):
+    return float(a - b)
+
+
+def multiply(a, b):
+    return float(a * b)
+
+
+def divide(a, b):
+    return float(a / b)
+
+
+def int_division(a, b):
+    return int(a // b)
+
+
+def mod(a, b):
+    return float(a % b)
